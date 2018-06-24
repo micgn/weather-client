8706738c7da5ed14688bbeba8a7608bd29e2a1b2
mgnatz <spa1@trali.de>
2018-06-24 12:41:12 +0200

    integrated dygraph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+declare var Dygraph: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+
+  ngOnInit(): void {
+
+    const data = `Date,A,B
+2016/01/01,10,20
+2016/07/01,20,10
+2016/12/31,40,30
+`;
+    const g = new Dygraph(document.getElementById('chart'), data, {
+      legend: 'always',
+      showRoller: false,
+      rollPeriod: 0,
+      customBars: false,
+      ylabel: 'Temperature (°C)',
+    });
+
+  }
 }
